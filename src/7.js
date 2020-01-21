36197e6642d441b5d8c7b0000fc0dc3b2afca1cc
hcyhehe <960696163@qq.com>
2020-01-21 16:02:13 +0800

    修改demo

--- src/7.js
+++ src/7.js
@@ -1,5 +1,5 @@
 /*
- * 闭包的一个简单例子 
+ * 闭包的简单例子 
  */
 
 function outer(){
@@ -11,3 +11,28 @@ function outer(){
 var b = outer()()  //因为return了一个函数，所以下面要调用两次
 console.log(b)  //1
 
+for (var i=1;i<=5;i++){
+    console.log('d:'+i)  //1,2,3,4,5
+}
+
+for (var i=1;i<=5;i++){
+    setTimeout(function timer(){
+        console.log('a:'+i)  //每隔一秒，打印6出来（全是6）
+    }, i*1000)
+}
+
+for (var i=1;i<=5;i++){
+    (function(i){
+        setTimeout(function timer(){
+            console.log('b:'+i)  //每隔一秒，分别打印出1,2,3,4,5
+        }, i*1000)
+    })(i)
+}
+
+for (var i=1;i<=5;i++){ 
+    setTimeout((function(i){
+        return function(){
+            console.log('c:'+i)  //每隔一秒，分别打印出1,2,3,4,5
+        }
+    })(i), i*1000)
+}
