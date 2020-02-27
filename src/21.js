a6b6e3e17a37bff6c90ce83d7785d538eab7a57e
hcyhehe <960696163@qq.com>
2020-02-28 00:40:56 +0800

    444444444

--- src/21.js
+++ src/21.js
@@ -1,17 +1,20 @@
 //catch没有捕捉到异常
-new Promise(()=>{
-    throw new Error('bye')
+new Promise((resolve, reject)=>{
+    //throw new Error('bye')
+    console.log(asdf)
+    reject('11111111')
 }).then(()=>{
     console.log('this is then')
 }).catch(error=>{
-    console.log('Error:',error.message) //没有打印
+    console.log('Error1:',error.message) //没有打印
 })
 
 //catch可以捕捉到异常
-new Promise(()=>{
-    console.log('nonono')
-}).then(()=>{
-    throw new Error('bye')
-}).catch(error=>{
-    console.log('Error:',error.message) //打印
-})
+// new Promise(()=>{
+//     console.log('nonono')
+// }).then(()=>{
+//     //throw new Error('bye')
+//     console.log(asdf)
+// }).catch(error=>{
+//     console.log('Error2:',error.message) //打印
+// })
